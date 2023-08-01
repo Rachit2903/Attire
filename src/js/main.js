f25771dd9ac4412b62a7ffbd7e1f7373e25cd641
Rachit2903 <97540447+Rachit2903@users.noreply.github.com>
2023-08-01 19:53:20 +0530

    Add files via upload

---
+++ src/js/main.js
@@ -0,0 +1,180 @@
+// init Isotope
+var $grid = $('.collection-list').isotope({
+    // options
+    filter:'.men'
+  });
+  // filter items on button click
+  $('.filter-button-group').on( 'click', 'button', function() {
+    var filterValue = $(this).attr('data-filter');
+    resetFilterBtns();
+    $(this).addClass('active-filter-btn');
+    $grid.isotope({ filter: filterValue });
+  });
+
+  var filterBtns=$(".filter-button-group").find("button");
+
+  function resetFilterBtns(){
+    filterBtns.each(function(){
+        $(this).removeClass("active-filter-btn");
+    })
+  }
+
+
+//   
+
+$('.filter-button-group').on( 'click', 'a', function() {
+    var filterValue = $(this).attr('data-filter');
+    resetFilterAnchor();
+    var buttonId=$(this).attr('href');
+    $($(this).attr('href')).addClass('active-filter-btn');
+    // console.log(buttonId);
+    // $(#buttonId).addClass('active-filter-btn');
+    $grid.isotope({ filter: filterValue });
+  });
+
+  var filterBtns=$(".filter-button-group").find("button");
+
+  function resetFilterAnchor(){
+    filterBtns.each(function(){
+        $(this).removeClass("active-filter-btn");
+    })
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
